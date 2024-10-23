92d6f28e1d7c6589e34474fa4b5b1ab0055198e4
ParrvLuthra <parrvluthra@gmail.com>
2024-10-23 17:04:08 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input) {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Minimal To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>✖</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
